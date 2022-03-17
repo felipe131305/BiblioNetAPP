b62ab4238983041b4fc086f3f83b411062892cdf
felipe131305 <felipe08119617@gmail.com>
2022-03-17 10:34:56 -0500

    firts commit

---
+++ BiblioNetAPP/Controllers/BookNameController.cs
@@ -0,0 +1,24 @@
+﻿using BiblioNetAPP.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BiblioNetAPP.Controllers
+{
+    public class BookNameController : Controller
+    {
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(BookNameModel bookModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(bookModel);
+            }
+            return View();
+
+        }
+    }
+}
---
+++ BiblioNetAPP/Models/BookNameModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BiblioNetAPP.Models
+{
+    public class BookNameModel
+    {
+
+        public int Id { get; set; }
+        [Required(ErrorMessage="{0} is required")]
+        public string bookName { get; set; }
+
+    }
+}
